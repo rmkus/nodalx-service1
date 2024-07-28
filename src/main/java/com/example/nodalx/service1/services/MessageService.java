143895888b4d4f25214a0e40e53fa44cd49b214d
Makesh Ramakrishnan <makesh.ramakrishnan@ventuor.com>
2024-07-28 11:00:14 -0400

    Separate queue for each service

--- src/main/java/com/example/nodalx/service1/services/MessageService.java
+++ src/main/java/com/example/nodalx/service1/services/MessageService.java
@@ -6,27 +6,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Service
 public class MessageService {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
-    private static final String EXCHANGE_NAME = "microserviceExchange";
-    private static final String ROUTING_KEY = "microserviceKey";
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    private static final String EXCHANGE_NAME = "NodalxExchange";
+    private static final String ROUTING_KEY_1 = "nodalxeKey1";
+    private static final String ROUTING_KEY_2 = "nodalxeKey2";
 
     @Scheduled(fixedDelay = 10000, initialDelay = 10000)
     public void sendMessage() {
-        System.out.println("Service1:Sending ping...");
+        System.out.println(LocalDateTime.now().format(formatter)+" - Service1:Sending ping...");
 
-        rabbitTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY, "ping");
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY_2, "ping");
     }
 
-    @RabbitListener(queues = "microserviceQueue")
+    @RabbitListener(queues = "nodalxQueue1")
     public void receiveMessage(String message) {
-        System.out.println("Service1:Received message: " + message);
+        System.out.println(LocalDateTime.now().format(formatter)+" - Service1:Received message: " + message);
         if (message.equals("ping")) {
-            System.out.println("Service1:Sending pong...");
-            rabbitTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY, "pong");
+            System.out.println(LocalDateTime.now().format(formatter)+" - Service1:Sending pong...");
+            rabbitTemplate.convertAndSend(EXCHANGE_NAME, ROUTING_KEY_2, "pong");
         }
     }
 }
